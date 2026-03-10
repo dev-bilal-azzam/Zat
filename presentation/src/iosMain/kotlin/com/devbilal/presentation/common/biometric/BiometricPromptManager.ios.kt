2803dd375224abaedcc15ff26e31d84d0a86d707
dev-bilal-azzam <bilal.azzam.dev@gmail.com>
2026-03-10 11:56:11 +0200

    finalize authentication flow for ios

--- presentation/src/iosMain/kotlin/com/devbilal/presentation/common/biometric/BiometricPromptManager.ios.kt
+++ presentation/src/iosMain/kotlin/com/devbilal/presentation/common/biometric/BiometricPromptManager.ios.kt
@@ -1,21 +1,54 @@
 package com.devbilal.presentation.common.biometric
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
 import org.koin.core.scope.Scope
+import platform.LocalAuthentication.LAContext
+import platform.LocalAuthentication.LAErrorBiometryNotAvailable
+import platform.LocalAuthentication.LAErrorBiometryNotEnrolled
+import platform.LocalAuthentication.LAPolicyDeviceOwnerAuthenticationWithBiometrics
 
-class IosBiometricPromptManager : BiometricPromptManager{
+class IosBiometricPromptManager : BiometricPromptManager {
+    private val resultChannel = Channel<BiometricResult>()
     override val promptResults: Flow<BiometricResult>
-        get() = TODO("Not yet implemented")
+        get() = resultChannel.receiveAsFlow()
 
+    @OptIn(ExperimentalForeignApi::class)
     override fun showBiometricPrompt(
         title: String,
         description: String,
         cancel: String
     ) {
-        TODO("Not yet implemented")
+        val context = LAContext()
+
+        val canEvaluate = context.canEvaluatePolicy(LAPolicyDeviceOwnerAuthenticationWithBiometrics, null)
+
+        if (!canEvaluate) {
+            resultChannel.trySend(BiometricResult.FeatureUnavailable)
+            return
+        }
+
+        context.localizedCancelTitle = cancel
+
+        context.evaluatePolicy(
+            policy = LAPolicyDeviceOwnerAuthenticationWithBiometrics,
+            localizedReason = description
+        ) { success, evaluationError ->
+            if (success) {
+                resultChannel.trySend(BiometricResult.AuthenticationSuccess)
+            } else {
+                when (evaluationError?.code) {
+                    LAErrorBiometryNotAvailable -> resultChannel.trySend(BiometricResult.HardwareUnavailable)
+                    LAErrorBiometryNotEnrolled -> resultChannel.trySend(BiometricResult.AuthenticationNotSet)
+                    else -> resultChannel.trySend(BiometricResult.AuthenticationFailed)
+                }
+            }
+        }
     }
 }
 
 actual fun Scope.createBiometricPromptManager(): BiometricPromptManager {
-    TODO("Not yet implemented")
+    return IosBiometricPromptManager()
 }
