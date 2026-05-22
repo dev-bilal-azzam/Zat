de7ecc9438af8b96cbac25d8d3657789e8fc2014
dev-bilal-azzam <bilal.azzam.dev@gmail.com>
2026-05-22 16:39:33 +0300

    view diary items list in home

--- presentation/src/commonMain/kotlin/com/devbilal/presentation/features/diary/screens/home/HomeContract.kt
+++ presentation/src/commonMain/kotlin/com/devbilal/presentation/features/diary/screens/home/HomeContract.kt
@@ -1,17 +1,27 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
 package com.devbilal.presentation.features.diary.screens.home
 
+import com.devbilal.domain.entity.DiaryEntrySummary
 import com.devbilal.presentation.base.UiEffect
 import com.devbilal.presentation.base.UiIntent
 import com.devbilal.presentation.base.UiState
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 data class HomeState(
-    val temp: String? = null
+    val entries: List<DiaryEntrySummary> = emptyList(),
+    val isLoading: Boolean = false
 ) : UiState
 
 sealed interface HomeIntent : UiIntent {
     data object OnBackClicked : HomeIntent
+    data class OnEntryClicked(val id: Uuid) : HomeIntent
+    data object OnAddEntryClicked : HomeIntent
 }
 
 sealed interface HomeEffect : UiEffect {
     data object NavigateBack : HomeEffect
+    data class NavigateToEditEntry(val id: Uuid) : HomeEffect
+    data object NavigateToAddEntry : HomeEffect
 }
--- presentation/src/commonMain/kotlin/com/devbilal/presentation/features/diary/screens/home/HomeViewModel.kt
+++ presentation/src/commonMain/kotlin/com/devbilal/presentation/features/diary/screens/home/HomeViewModel.kt
@@ -1,18 +1,38 @@
 package com.devbilal.presentation.features.diary.screens.home
 
+import androidx.lifecycle.viewModelScope
+import com.devbilal.domain.usecase.diary.GetAllDiaryEntriesUseCase
 import com.devbilal.presentation.base.BaseViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlin.uuid.ExperimentalUuidApi
 
-class HomeViewModel : BaseViewModel<HomeState, HomeIntent, HomeEffect>(
+@OptIn(ExperimentalUuidApi::class)
+class HomeViewModel(
+    private val getAllDiaryEntriesUseCase: GetAllDiaryEntriesUseCase
+) : BaseViewModel<HomeState, HomeIntent, HomeEffect>(
     HomeState()
 ) {
 
     init {
+        getAllEntries()
+    }
+
+    private fun getAllEntries() {
+        getAllDiaryEntriesUseCase()
+            .onStart { updateState { copy(isLoading = true) } }
+            .onEach { entries ->
+                updateState { copy(entries = entries, isLoading = false) }
+            }
+            .launchIn(viewModelScope)
     }
 
     override fun handleIntent(intent: HomeIntent) {
         when (intent) {
-            // handle all of your Intents here
-            else -> TODO()
+            HomeIntent.OnBackClicked -> sendEffect(HomeEffect.NavigateBack)
+            HomeIntent.OnAddEntryClicked -> sendEffect(HomeEffect.NavigateToAddEntry)
+            is HomeIntent.OnEntryClicked -> sendEffect(HomeEffect.NavigateToEditEntry(intent.id))
         }
     }
 }
